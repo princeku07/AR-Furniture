37874051482c3cb54185c5a4017718c9fcbfa0f2
princeku07 <princekumarsingh7007@gmail.com>
2023-07-08 16:37:44 +0530

    VideoNode added

--- app/src/main/java/com/xperiencelabs/arapp/MainActivity.kt
+++ app/src/main/java/com/xperiencelabs/arapp/MainActivity.kt
@@ -1,22 +1,32 @@
 package com.xperiencelabs.arapp
 
 import android.content.Context
+import android.graphics.BitmapFactory
+import android.media.MediaPlayer
+import android.net.Uri
 import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.core.view.isGone
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
+import com.google.ar.core.Config
 import io.github.sceneview.ar.ArSceneView
-import io.github.sceneview.ar.arcore.LightEstimationMode
 import io.github.sceneview.ar.node.ArModelNode
+import io.github.sceneview.ar.node.AugmentedImageNode
 import io.github.sceneview.ar.node.PlacementMode
+import io.github.sceneview.material.setExternalTexture
 import io.github.sceneview.math.Position
+import io.github.sceneview.math.Rotation
+import io.github.sceneview.node.VideoNode
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var sceneView: ArSceneView
+    private lateinit var sceneView: ArSceneView
     lateinit var placeButton: ExtendedFloatingActionButton
     private lateinit var modelNode: ArModelNode
+    private lateinit var videoNode: VideoNode
+    private lateinit var mediaPlayer:MediaPlayer
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,17 +34,22 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         sceneView = findViewById<ArSceneView?>(R.id.sceneView).apply {
-            this.lightEstimationMode = LightEstimationMode.ENVIRONMENTAL_HDR_NO_REFLECTIONS
+            this.lightEstimationMode = Config.LightEstimationMode.DISABLED
         }
+
+        mediaPlayer = MediaPlayer.create(this,R.raw.ad)
 
         placeButton = findViewById(R.id.place)
 
         placeButton.setOnClickListener {
             placeModel()
-
         }
 
-        modelNode = ArModelNode(PlacementMode.INSTANT).apply {
+        videoNode = VideoNode(sceneView.engine, scaleToUnits = 0.7f, centerOrigin = Position(y=-4f), glbFileLocation = "models/plane.glb", player = mediaPlayer, onLoaded = {_,_ ->
+            mediaPlayer.start()
+        })
+
+        modelNode = ArModelNode(sceneView.engine,PlacementMode.INSTANT).apply {
             loadModelGlbAsync(
                 glbFileLocation = "models/sofa.glb",
                 scaleToUnits = 1f,
@@ -43,6 +58,7 @@ class MainActivity : AppCompatActivity() {
             )
             {
                 sceneView.planeRenderer.isVisible = true
+                val materialInstance = it.materialInstances[0]
             }
             onAnchorChanged = {
                 placeButton.isGone = it != null
@@ -50,12 +66,24 @@ class MainActivity : AppCompatActivity() {
 
         }
         sceneView.addChild(modelNode)
+        modelNode.addChild(videoNode)
 
     }
 
    private fun placeModel(){
        modelNode.anchor()
+
        sceneView.planeRenderer.isVisible = false
 
    }
+
+    override fun onPause() {
+        super.onPause()
+        mediaPlayer.stop()
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        mediaPlayer.release()
+    }
+
 }
